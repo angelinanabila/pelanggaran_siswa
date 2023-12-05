9b6e6c63a5ad4395af633c387e6cd2edfad63228
nabila <angelinanabila459@gmail.com>
2023-12-06 06:15:45 +0700

    first commit

---
+++ pelanggaran_siswa.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Des 2023 pada 00.10
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pelanggaran_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_pelanggaran_siswa`
+--
+-- Kesalahan membaca struktur untuk tabel pelanggaran_siswa.detail_pelanggaran_siswa: #1932 - Table &#039;pelanggaran_siswa.detail_pelanggaran_siswa&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel pelanggaran_siswa.detail_pelanggaran_siswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pelanggaran_siswa`.`detail_pelanggaran_siswa`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggaran`
+--
+-- Kesalahan membaca struktur untuk tabel pelanggaran_siswa.pelanggaran: #1932 - Table &#039;pelanggaran_siswa.pelanggaran&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel pelanggaran_siswa.pelanggaran: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pelanggaran_siswa`.`pelanggaran`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggaran_siswa`
+--
+-- Kesalahan membaca struktur untuk tabel pelanggaran_siswa.pelanggaran_siswa: #1932 - Table &#039;pelanggaran_siswa.pelanggaran_siswa&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel pelanggaran_siswa.pelanggaran_siswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pelanggaran_siswa`.`pelanggaran_siswa`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+-- Kesalahan membaca struktur untuk tabel pelanggaran_siswa.siswa: #1932 - Table &#039;pelanggaran_siswa.siswa&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel pelanggaran_siswa.siswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pelanggaran_siswa`.`siswa`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+-- Kesalahan membaca struktur untuk tabel pelanggaran_siswa.user: #1932 - Table &#039;pelanggaran_siswa.user&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel pelanggaran_siswa.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pelanggaran_siswa`.`user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
